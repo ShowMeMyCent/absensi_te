4556598f2f5e0cad3150b446fc6040dd9c91316e
Nevermind0506 <davidmaulanaibrahim1@gmail.com>
2023-05-26 09:38:59 +0700

    adw

--- lib/app/modules/pengajuan/controllers/pengajuan_controller.dart
+++ lib/app/modules/pengajuan/controllers/pengajuan_controller.dart
@@ -3,10 +3,62 @@ import 'package:get/get.dart';
 
 class PengajuanController extends GetxController {
   var selectedJenis = "".obs;
-  TextEditingController valDate = TextEditingController();
-  TextEditingController valKeterangan = TextEditingController();
+  //Cuti
+  var valDdJenisCuti = "".obs;
+  var valDdPenggantiCuti = "".obs;
 
-  onChangeJenis(var jenis) {
-    selectedJenis.value = jenis;
-  }
+  List<String> jenisCuti = [
+    "Tahunan",
+    "Melahirkan",
+    "Menikah",
+    "Duka",
+    "Kelahiran",
+    "Lainnya",
+  ];
+
+  TextEditingController valDateAwalCuti = TextEditingController();
+  TextEditingController valDateAkhirCuti = TextEditingController();
+  TextEditingController valKeteranganCuti = TextEditingController();
+
+  // Izin
+  var valDdJenisIzin = "".obs;
+  var valDdPenggantiIzin = "".obs;
+
+  List<String> jenisIzin = [
+    "Tidak Masuk Kerja",
+    "Tidak Mengabsen",
+    "Datang Terlambat",
+    "Pulang Lebih Awal",
+    "Meninggalkan Kerja Selama Jam Kerja",
+    "Lainnya",
+  ];
+
+  TextEditingController valDateAwalIzin = TextEditingController();
+  TextEditingController valDateAkhirIzin = TextEditingController();
+  TextEditingController valKeteranganIzin = TextEditingController();
+
+  // Dinas
+  List<String> personil = [
+    "David Maulana",
+    "Wildan Khalid",
+    "Anargya Prabaswara",
+    "Surya Rizky",
+    "Muhammad Raffa",
+    "Nanda Arifa",
+  ];
+
+  var valNamaPersonil = "".obs;
+  TextEditingController valPerihalDinas = TextEditingController();
+  TextEditingController valDateAwalDinas = TextEditingController();
+  TextEditingController valDateAkhirDinas = TextEditingController();
+
+  // Global
+  List<String> pengganti = [
+    "David Maulana",
+    "Wildan Khalid",
+    "Anargya Prabaswara",
+    "Surya Rizky",
+    "Muhammad Raffa",
+    "Nanda Arifa",
+  ];
 }
